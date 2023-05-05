2d994942afe5af4f8081d6a89b962d0e2437c87d
Wzz0725 <823710946@qq.com>
2023-05-05 13:03:43 +0800

    在main,js里引入并使用elementui和buefy

--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+//Buefy
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'//导入样式
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
+Vue.use(Buefy)
 
 new Vue({
   router,
