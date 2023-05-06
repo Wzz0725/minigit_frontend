e65500c70a536f58f05fedc0556be352bc177fb9
Wzz0725 <823710946@qq.com>
2023-05-07 03:45:08 +0800

    取消src/store/getters.js的注释

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,5 +1,5 @@
-/* const getters = {
+const getters = {
     token: state => state.user.token,   // token
     user: state => state.user.user,     // 用户对象
   }
-  export default getters */
+  export default getters
